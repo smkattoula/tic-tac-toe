ba5df17c336dd1b7ed0a5e38d3683297b29f5d87
Shaker Kattoula <smkattoula@gmail.com>
2021-09-21 19:00:19 -0700

    create Restart component with functionality to allow players to play again, create functions to calculate winner, check if board is full in the case of a draw, and to get the status of who's turn it is and who wins, render Restart component inside of Game component, refactor code

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,18 +1,84 @@
 import React, { useState } from "react";
+import Restart from "./Restart";
 import Square from "./Square";
 
 const Game = () => {
   const [squares, setSquares] = useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = useState(true);
+  const nextSymbol = isXNext ? "X" : "O";
+
+  const calculateWinner = (squares) => {
+    const possibleLines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (let i = 0; i < possibleLines.length; i++) {
+      const [a, b, c] = possibleLines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  };
+
+  const winner = calculateWinner(squares);
 
   const renderSquare = (i) => {
     return (
       <Square
         value={squares[i]}
         onClick={() => {
+          if (squares[i] != null || winner != null) {
+            return;
+          }
+
           const nextSquares = squares.slice();
-          nextSquares[i] = "X";
+          nextSquares[i] = nextSymbol;
           setSquares(nextSquares);
-          console.log(nextSquares);
+
+          setIsXNext(!isXNext);
+        }}
+      />
+    );
+  };
+
+  const isBoardFull = (squares) => {
+    squares.map((square) => {
+      if (square == null) {
+        return false;
+      } else {
+        return true;
+      }
+    });
+  };
+
+  const getStatus = () => {
+    if (winner) {
+      return "Winner: " + winner;
+    } else if (isBoardFull(squares)) {
+      return "Draw!";
+    } else {
+      return "Next player: " + nextSymbol;
+    }
+  };
+
+  const renderRestartButton = () => {
+    return (
+      <Restart
+        onClick={() => {
+          setSquares(Array(9).fill(null));
+          setIsXNext(true);
         }}
       />
     );
@@ -38,6 +104,8 @@ const Game = () => {
             {renderSquare(8)}
           </div>
         </div>
+        <div className="game-info">{getStatus()}</div>
+        <div className="restart-button">{renderRestartButton()}</div>
       </div>
     </div>
   );
---
+++ src/components/Restart.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Restart = ({ onClick }) => {
+  return (
+    <button className="restart" onClick={onClick}>
+      Play Again
+    </button>
+  );
+};
+
+export default Restart;
